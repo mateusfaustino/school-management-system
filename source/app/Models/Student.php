dd895827b1f924e8c77440630ccc25b431a479d1
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-09-28 15:08:34 -0300

    make Studendt Model

--- source/app/Models/Student.php
+++ source/app/Models/Student.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'birth_date',
+        'gender'
+    ];
 }
