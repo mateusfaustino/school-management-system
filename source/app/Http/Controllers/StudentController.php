8cd601fe23c828ce62d4d9c9786e05cc701fd67c
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-09-28 16:10:25 -0300

    create index feature

--- source/app/Http/Controllers/StudentController.php
+++ source/app/Http/Controllers/StudentController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Student;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = Student::all();
+        $students_count = count($students);
+        $http_code = 200;
+        
+        if($students_count==0){
+            $message ='Não há estudantes cadastrados';
+        }else{
+            $message = 'Lista de todos(as) os(as) estudantes';
+        }
+        return response([
+            'data'=>$students,
+            'message'=>$message,
+            'count'=>$students_count
+        ], $http_code);
+    }
 }
--- source/routes/api.php
+++ source/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
 
 /* 
 |--------------------------------------------------------------------------
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/students', [StudentController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
